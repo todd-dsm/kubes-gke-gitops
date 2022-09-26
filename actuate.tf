5612788d89a767f69460c11739d0734a7ee6f46e
Todd E Thomas <todd.dsm@gmail.com>
2022-09-25 19:11:17 -0700

    switched to modules; much better

--- actuate.tf
+++ actuate.tf
@@ -1,40 +1,74 @@
 # -----------------------------------------------------------------------------
+# Base Network Configuration
+# -----------------------------------------------------------------------------
+module "vpc" {
+  source  = "terraform-google-modules/network/google"
+  version = "5.2.0"
+
+  project_id   = var.project_id
+  network_name = var.network
+  routing_mode = "GLOBAL"
+
+  subnets = [
+    {
+      subnet_name   = var.subnetwork
+      subnet_ip     = var.subnetwork_ip_range
+      subnet_region = var.region
+    }
+  ]
+
+  secondary_ranges = {
+    (var.subnetwork) = [
+      {
+        range_name    = var.ip_range_pods
+        ip_cidr_range = var.ip_range_pods_cidr
+      },
+      {
+        range_name    = var.ip_range_services
+        ip_cidr_range = var.ip_range_services_cidr
+      }
+    ]
+  }
+}
+# -----------------------------------------------------------------------------
 # Kubernetes Cluster for Applications
 # -----------------------------------------------------------------------------
-module "apps" {
-  source        = "./mods/apps"
-  envBuild      = var.envBuild
-  region        = var.region
-  zone          = var.zone
-  project_id    = var.project_id
-  cidr_range    = var.cidr_range
-  cluster_apps  = var.cluster_apps
-  worker_type   = var.worker_type
-  min_dist_size = var.min_dist_size
-  dns_name      = var.dns_name
-  # dns_zone      = var.dns_zone
+module "gke" {
+  source             = "terraform-google-modules/kubernetes-engine/google"
+  version            = "23.1.0"
+  project_id         = var.project_id
+  name               = var.cluster_name
+  regional           = true
+  region             = var.region
+  release_channel    = "REGULAR"
+  network            = module.vpc.network_name
+  subnetwork         = module.vpc.subnets_names[0]
+  ip_range_pods      = var.ip_range_pods
+  ip_range_services  = var.ip_range_services
+  network_policy     = false
+  cluster_dns_domain = var.dns_name
+  identity_namespace = "enabled"
+  # ------------------ TEST ---------------------------------------------------
+  # cluster_dns_provider    = "CLOUD_DNS"
+  # cluster_dns_scope       = "VPC_SCOPE"
+  # cluster_resource_labels = { "mesh_id" : "proj-${var.project_number}" }
+  # cluster_autoscaling = {
+  #   enabled             = true
+  #   autoscaling_profile = "BALANCED"
+  #   min_cpu_cores       = 1
+  #   max_cpu_cores       = 100
+  #   min_memory_gb       = 1
+  #   max_memory_gb       = 1000
+  #   gpu_resources       = []
+  # }
+  # ------------------ TEST ---------------------------------------------------
+  node_pools = [
+    {
+      name         = "${var.cluster_name}-np"
+      autoscaling  = true
+      auto_upgrade = true
+      node_count   = 4
+      machine_type = var.worker_type
+    },
+  ]
 }
-
-# -----------------------------------------------------------------------------
-# Kubernetes Cluster for Vault
-# -----------------------------------------------------------------------------
-//module "vault" {
-//  source        = "./mods/vault"
-//  envBuild      = var.envBuild
-//  cluster_vault  = var.cluster_vault
-//  zone          = var.zone
-//  worker_type = "e2-medium"
-//  project       = var.project
-//  region        = var.region
-//  dns_zone      = var.dns_zone
-//  min_dist_size   = 1
-//}
-
-# -----------------------------------------------------------------------------
-# test module
-# -----------------------------------------------------------------------------
-//module "tester" {
-//  source   = "./mods/test"
-//  dns_zone = var.dns_zone
-//  envBuild = var.envBuild
-//}
