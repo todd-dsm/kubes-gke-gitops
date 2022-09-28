e10757cc8eb3d79ae37493d14d36c7e3511de9e2
Todd E Thomas <todd.dsm@gmail.com>
2022-09-28 10:58:42 -0700

    testing some workload-id stuff

--- actuate.tf
+++ actuate.tf
@@ -72,3 +72,63 @@ module "gke" {
     },
   ]
 }
+
+# -----------------------------------------------------------------------------
+# Policy Bindings for Additional Applications
+# We'll just add to the node-pool service account created in the gke module.
+# TEST before/after with: gcloud projects get-iam-policy $myProject | grep $roleName
+# Example: gcloud projects get-iam-policy $myProject | grep dns.admin
+# -----------------------------------------------------------------------------
+/*
+  Permissions for External DNS; using the Worker Node Service Account method
+  REF: https://github.com/kubernetes-sigs/external-dns/blob/master/docs/tutorials/gke.md#worker-node-service-account-method
+  Docs: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project_iam#google_project_iam_binding
+*/
+resource "google_project_iam_binding" "worker_sa" {
+  project = var.project_id
+  role    = "roles/dns.admin"
+
+  members = [
+    "serviceAccount:${module.gke.service_account}",
+  ]
+}
+
+/*
+  Create a Workload Identity Pairing
+  GCP: https://cloud.google.com/kubernetes-engine/docs/how-to/workload-identity
+  TF:  https://registry.terraform.io/modules/terraform-google-modules/kubernetes-engine/google/latest/submodules/workload-identity
+*/
+
+# # Pre-existing Kubernetes SA; comes from Helm install
+# resource "kubernetes_service_account" "external_dns" {
+#   metadata {
+#     name      = "external-dns"
+#     namespace = "default"
+#   }
+# }
+
+# # Pre-existing Kubernetes SA; comes from node-pool creation
+# resource "google_service_account" "preexisting" {
+#   account_id   = "external-dns-sax"
+# }
+
+# module "xdns-workload-identity" {
+#   source     = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+#   version    = "23.1.0"
+#   name       = "external-dns"
+#   namespace  = "default"
+#   project_id = var.project_id
+#   roles      = ["roles/storage.admin", "roles/compute.admin"]
+# }
+
+
+# module "xdns-workload-identity" {
+#   source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+#   use_existing_gcp_sa = true
+#   name                = google_service_account.preexisting.account_id
+#   project_id          = var.project_id
+
+#   # wait for the custom GSA to be created to force module data source read during apply
+#   # https://github.com/terraform-google-modules/terraform-google-kubernetes-engine/issues/1059
+#   depends_on = [google_service_account.preexisting]
+# }
