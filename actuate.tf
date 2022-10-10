22a5aab1103abdc33c3c8fa85534b9ba20ed05b9
Todd E Thomas <todd.dsm@gmail.com>
2022-10-10 15:25:05 -0700

    clean-up

--- actuate.tf
+++ actuate.tf
@@ -1,49 +1,12 @@
 # -----------------------------------------------------------------------------
-# Kubernetes Cluster for Applications
+# Base Network Configuration
 # -----------------------------------------------------------------------------
-# REF: https://github.com/hashicorp/learn-terraform-provision-gke-cluster
-# DOC: https://learn.hashicorp.com/tutorials/terraform/gke
-
-module "vpc" {
-  source  = "terraform-google-modules/network/google//examples/simple_project_with_regional_network"
-  version = "5.2.0"
-  # insert the 2 required variables here
+# there are bugz: https://github.com/terraform-google-modules/terraform-google-network/issues/383
+# otherwise I'd just use the module
+module "network" {
+  source        = "./mods/network"
+  cidr_range    = var.cidr_range
+  min_dist_size = var.min_dist_size
+  project_id    = var.project_id
+  region        = var.region
 }
-
-#module "apps" {
-#  source        = "./mods/apps"
-#  envBuild      = var.envBuild
-#  region        = var.region
-#  project_id    = var.project_id
-#  cluster_apps  = var.cluster_apps
-#  worker_type   = var.worker_type
-#  min_dist_size = var.min_dist_size
-#  dns_name      = var.dns_name
-#  cidr_range    = var.cidr_range
-#  xdnsSA        = var.xdnsSA
-#  xdns_key      = var.xdns_key
-#}
-
-# -----------------------------------------------------------------------------
-# Kubernetes Cluster for Vault
-# -----------------------------------------------------------------------------
-//module "vault" {
-//  source        = "./mods/vault"
-//  envBuild      = var.envBuild
-//  cluster_vault  = var.cluster_vault
-//  zone          = var.zone
-//  worker_type = "e2-medium"
-//  project       = var.project
-//  region        = var.region
-//  dns_zone      = var.dns_zone
-//  min_dist_size   = 1
-//}
-
-# -----------------------------------------------------------------------------
-# test module
-# -----------------------------------------------------------------------------
-//module "tester" {
-//  source   = "./mods/test"
-//  dns_zone = var.dns_zone
-//  envBuild = var.envBuild
-//}
