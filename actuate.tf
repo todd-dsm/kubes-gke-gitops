47bd04c3fc078792fd2db5103c8e11299f03e4d8
Todd E Thomas <todd.dsm@gmail.com>
2022-10-13 10:38:09 -0700

    added cluster definition

--- actuate.tf
+++ actuate.tf
@@ -1,67 +1,43 @@
-# -----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
 # Kubernetes Cluster for Applications
-# -----------------------------------------------------------------------------
-#resource "google_container_cluster" "apps" {
-#  name             = var.project_id
-#  location         = var.region
-#  enable_autopilot = true
-#
-#  network    = module.network.network_name
-#  subnetwork = module.network.subnets_names[0]
-#}
+# TFR: https://registry.terraform.io/modules/terraform-google-modules/kubernetes-engine/google/latest
+# TFD: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/container_cluster
+# GCP: https://cloud.google.com/kubernetes-engine/docs/concepts/cluster-architecture (Official Docs)
+# GCP: https://cloud.google.com/kubernetes-engine/docs/reference/rest/v1beta1/projects.locations.clusters (api refs)
+# ----------------------------------------------------------------------------------------------------------------------
+module "apps_cluster" {
+  source                    = "terraform-google-modules/kubernetes-engine/google"
+  project_id                = module.network.project_id
+  name                      = "${var.project_id}-apps"
+  regional                  = true
+  region                    = var.region
+  network                   = module.network.network_name
+  subnetwork                = var.subnet_name
+  ip_range_pods             = "${var.project_id}-pods"
+  ip_range_services         = "${var.project_id}-svcs"
+  default_max_pods_per_node = 64
+  network_policy            = true
+  release_channel           = "REGULAR"
+  cluster_resource_labels   = { "mesh_id" : "proj-${data.google_project.project.number}" }
+  node_pools = [
+    {
+      name         = "${var.project_id}-apps-np"
+      autoscaling  = true
+      auto_upgrade = true
+      min_count    = 1
+      max_count    = 5
+      node_count   = 2
+      machine_type = "e2-standard-4"
+    },
+  ]
+}
 
-
-#resource "google_container_cluster" "apps" {
-#  #provider = google-beta
-#  name             = var.project_id
-#  location         = var.region
-#  enable_autopilot = true
-#
-#  network    = module.network.network_name
-#  subnetwork = module.network.subnets_names[0]
-#
-#  #  private_cluster_config {
-#  #    enable_private_endpoint = false
-#  #    enable_private_nodes    = true
-#  #    master_ipv4_cidr_block  = var.subnetwork_ip_range
-#  #  }
-#
-#  #  master_authorized_networks_config {
-#  #    dynamic "cidr_blocks" {
-#  #      for_each = var.authorized_source_ranges
-#  #      content {
-#  #        cidr_block = cidr_blocks.value
-#  #      }
-#  #    }
-#  #  }
-#
-#  maintenance_policy {
-#    recurring_window {
-#      start_time = "2021-06-18T00:00:00Z"
-#      end_time   = "2050-01-01T04:00:00Z"
-#      recurrence = "FREQ=WEEKLY"
-#    }
-#  }
-#
-#  # Configuration of cluster IP allocation for VPC-native clusters
-#  ip_allocation_policy {
-#    cluster_secondary_range_name  = "pods"
-#    services_secondary_range_name = "services"
-#  }
-#
-#  # Configuration options for the Release channel feature, which provide more control over automatic upgrades of your GKE clusters.
-#  release_channel {
-#    channel = "REGULAR"
-#  }
-#}
-
-
-# -----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
 # Base Network Configuration
 # TFD: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_network
 # TFD: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_subnetwork
 # TFR: https://registry.terraform.io/modules/terraform-google-modules/network/google/latest
-# -----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
 module "network" {
   source  = "terraform-google-modules/network/google"
   version = "5.2.0"
@@ -91,4 +67,11 @@ module "network" {
       },
     ]
   }
+}
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Discovery
+# ----------------------------------------------------------------------------------------------------------------------
+data "google_project" "project" {
+  project_id = var.project_id
 }
